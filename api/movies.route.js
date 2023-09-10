d8c9744ef419395e74ab02df9973a0840b3068f1
Tomas Paul Cservenak <tomas.cservenak@gmail.com>
2023-09-10 13:02:39 -0600

    adds review

--- api/movies.route.js
+++ api/movies.route.js
@@ -1,6 +1,11 @@
-import express from 'express'
-import MoviesController from './movies.controller.js';
-const router = express.Router() // get access to express router
-router.route('/').get(MoviesController.apiGetMovies)
+import express from "express";
+import MoviesController from "./movies.controller.js";
+import ReviewsController from "./reviews.controller.js";
+const router = express.Router(); // get access to express router
+router.route("/").get(MoviesController.apiGetMovies);
+router 
+  .route("/review") // this is the endpoint for the review
+  .post(ReviewsController.apiPostReview) // this is the method that will be called when a post request is made to the endpoint
+  .put(ReviewsController.apiUpdateReview) // this is the method that will be called when a put request is made to the endpoint
+  .delete(ReviewsController.apiDeleteReview); // this is the method that will be called when a delete request is made to the endpoint
 export default router;
-
